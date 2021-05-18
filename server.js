7063ae70aac0b8f2480f1ebeb055ba077accf25a
Bruno Bilandzic <bruno.bilandzic2@gmail.com>
2021-05-18 14:47:01 +0200

    Path resolve debug

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use("/api/quests", require("./routes/api/quest"));
 app.use("/api/answers", require("./routes/api/answers"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../client/build/static", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
