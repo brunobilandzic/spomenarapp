bc61660da0cbc24c97ffd1c935c6b99afb599f0c
Bruno Bilandzic <bruno.bilandzic2@gmail.com>
2021-05-26 16:08:08 +0200

    Update .env.examaple

--- config/index.js
+++ config/index.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
-  PWD_RESET_SALT: process.env.PWD_RESET_SALT,
   EMAIL: process.env.EMAIL,
   EMAIL_PASS: process.env.EMAIL_PASS,
   CLOUD_NAME: process.env.CLOUD_NAME,
